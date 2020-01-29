9a4e6c43702cb2179715ee820c97d0bb2ea8dcfd
Dmitrii Leliuhin <dleliuhin@gmail.com>
2020-01-30 01:30:14 +0300

    Feature. Rebase text file. Set num threads for text processing. Add static counter variable.

--- lab_6/main.cpp
+++ lab_6/main.cpp
@@ -1,16 +1,32 @@
-#include <omp.h>
+/*
+ * 12. Охранное агентство разработало новую систему управления элек-
+ * тронными замками. Для открытия двери клиент обязан произнести произ-
+ * вольную фразу из 25 слов. В этой фразе должно встречаться заранее огово-
+ * ренное слово, причем только один раз. Требуется создать OpenMP-
+ * приложение, управляющее замком. Потоки должны осуществлять сравнение
+ * параллельно по 5 слов.
+ */
+//=======================================================================================
 
+#include <omp.h>
+#include <QStringList>
 #include <QCoreApplication>
 #include <QDebug>
 #include <QFile>
+#include <QtMath>
 
-static constexpr auto key = "know";
-
+//=======================================================================================
 int main( int argc, char **argv )
 {
     QCoreApplication qapp( argc, argv );
 
-    QVector<QString> words;
+    //-----------------------------------------------------------------------------------
+
+    static constexpr auto key = "know";
+    static auto key_count = 0;
+    static auto words_per_thread = 5;
+
+    //-----------------------------------------------------------------------------------
 
     QFile conf_file( "../lab_6/conf.txt" );
 
@@ -20,20 +36,27 @@ int main( int argc, char **argv )
     QTextStream qts( &conf_file );
     qts.setCodec( "UTF-8" );
 
-    while ( !qts.atEnd() )
-        words.push_back( qts.readLine() );
+    auto words = qts.readAll().split( " " );
 
     conf_file.close();
 
+    qDebug() << "Key word: " << key;
     qDebug() << words;
 
-    #pragma omp parallel
-    {
-                auto count = omp_get_thread_num();
-                auto ItsMe = omp_get_num_threads();
+    omp_set_num_threads( qCeil( words.size() / qreal( words_per_thread ) ) );
 
-                qDebug( "Hello, OpenMP! I am %d of %d\n", count, ItsMe );
+#pragma omp parallel reduction( + : key_count )
+    {
+        auto count = omp_get_thread_num();
+        auto ItsMe = omp_get_num_threads();
+
+        key_count++;
+
+        qDebug( "Hello, OpenMP! I am %d of %d\n", count, ItsMe );
     }
+
+    qDebug() << key_count;
 
     return qapp.exec();
 }
+//=======================================================================================
