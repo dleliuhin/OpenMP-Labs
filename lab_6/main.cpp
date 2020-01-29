9353b30effcaa9eff4b124afda24b8186fb1e408
Dmitrii Leliuhin <dleliuhin@gmail.com>
2020-01-29 10:49:41 +0300

    Feature. Parse file contains 25 words.

--- lab_6/main.cpp
+++ lab_6/main.cpp
@@ -1,13 +1,39 @@
 #include <omp.h>
-#include <stdio.h>
 
-int main(int argc, char** argv)
+#include <QCoreApplication>
+#include <QDebug>
+#include <QFile>
+
+static constexpr auto key = "know";
+
+int main( int argc, char **argv )
 {
-#pragma omp parallel
+    QCoreApplication qapp( argc, argv );
+
+    QVector<QString> words;
+
+    QFile conf_file( "../lab_6/conf.txt" );
+
+    if ( !conf_file.open( QIODevice::ReadOnly | QIODevice::Text ) )
+        qCritical() << "Error cannot open file:" << &conf_file;
+
+    QTextStream qts( &conf_file );
+    qts.setCodec( "UTF-8" );
+
+    while ( !qts.atEnd() )
+        words.push_back( qts.readLine() );
+
+    conf_file.close();
+
+    qDebug() << words;
+
+    #pragma omp parallel
     {
-        auto count = omp_get_thread_num();
-        auto ItsMe = omp_get_num_threads();
-        printf("Hello, OpenMP! I am %d of %d\n", count, ItsMe );
+                auto count = omp_get_thread_num();
+                auto ItsMe = omp_get_num_threads();
+
+                qDebug( "Hello, OpenMP! I am %d of %d\n", count, ItsMe );
     }
-    return 0;
+
+    return qapp.exec();
 }
