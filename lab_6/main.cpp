214e5801083742dd51668f09ead94942491c23dc
Dmitrii Leliuhin <dleliuhin@gmail.com>
2020-02-02 13:54:23 +0300

    Feature. Implement lab 6. Add cfg files .ini - for params, .txt - for text.

--- lab_6/main.cpp
+++ lab_6/main.cpp
@@ -8,27 +8,63 @@
  */
 //=======================================================================================
 
-#include <omp.h>
 #include <QStringList>
 #include <QCoreApplication>
 #include <QDebug>
 #include <QFile>
 #include <QtMath>
+#include <QSettings>
+
+#include <omp.h>
+
+
+//=======================================================================================
+struct Params
+{
+    QString key;
+    int words_per_thread;
+};
+//=======================================================================================
 
 //=======================================================================================
 int main( int argc, char **argv )
 {
     QCoreApplication qapp( argc, argv );
 
-    //-----------------------------------------------------------------------------------
-
-    static constexpr auto key = "know";
     static auto key_count = 0;
-    static auto words_per_thread = 5;
 
     //-----------------------------------------------------------------------------------
 
-    QFile conf_file( "../lab_6/conf.txt" );
+    auto qargs = qapp.arguments();
+
+    if ( !qargs.contains( "-c" ) )
+        qWarning() << "The arguments doesn't contains flag -c!";
+
+    auto cfg_path = qargs.at( qargs.indexOf( "-c" ) + 1 );
+
+    if ( !qargs.contains( "-p" ) )
+        qWarning() << "The arguments doesn't contains flag -p!";
+
+    auto param_path = qargs.at( qargs.indexOf( "-p" ) + 1 );
+
+    //-----------------------------------------------------------------------------------
+
+    auto settings = new QSettings( param_path, QSettings::IniFormat, &qapp );
+
+    if ( settings->status() != QSettings::NoError )
+        qCritical() << "No config specified for path: ";
+
+    Params params;
+    {
+        settings->beginGroup( "params" );
+        params.key = settings->value( "key", "" ).toString();
+        params.words_per_thread = settings->value( "words_per_thread", 0 ).toInt();
+        settings->endGroup();
+    }
+
+    //-----------------------------------------------------------------------------------
+
+    QFile conf_file( cfg_path, &qapp );
 
     if ( !conf_file.open( QIODevice::ReadOnly | QIODevice::Text ) )
         qCritical() << "Error cannot open file:" << &conf_file;
@@ -37,25 +73,34 @@ int main( int argc, char **argv )
     qts.setCodec( "UTF-8" );
 
     auto words = qts.readAll().split( " " );
+    words.last().chop(1);
 
     conf_file.close();
 
-    qDebug() << "Key word: " << key;
-    qDebug() << words;
+    qDebug() << "Key word: " << params.key;
+    qDebug() << "Code phrase: " << words;
 
-    omp_set_num_threads( qCeil( words.size() / qreal( words_per_thread ) ) );
+    //-----------------------------------------------------------------------------------
+
+    omp_set_num_threads( qCeil( words.size() / qreal( params.words_per_thread ) ) );
 
 #pragma omp parallel reduction( + : key_count )
     {
-        auto count = omp_get_thread_num();
-        auto ItsMe = omp_get_num_threads();
+        auto thr_num = omp_get_thread_num();
 
-        key_count++;
+        auto subwords = words.mid( thr_num * params.words_per_thread,
+                                   params.words_per_thread );
 
-        qDebug( "Hello, OpenMP! I am %d of %d\n", count, ItsMe );
+        for ( const auto& word: subwords )
+            if ( word.compare( params.key ) == 0 )
+                key_count++;
     }
 
+    //-----------------------------------------------------------------------------------
+
     qDebug() << key_count;
+
+    qDebug() << ( key_count == 1 ? "Unlocked" : "Locked" );
 
     return qapp.exec();
 }
